06703e21756dd0987cb9807fc13008e98fdb526f
amir-dktor-jani <127782966+amir-dktor-jani@users.noreply.github.com>
2023-03-13 21:40:34 +0330

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,68 @@
+import sys
+import time
+import requests
+import os
+os.system("pip install requests")
+red='\033[31m'  #قرمز
+green='\033[32m' #سبز
+blue='\033[36m' #ابی
+pink='\033[35m'  #بنفش
+rang='\033[34m' #ابی تیره
+yellow='/033[93m'  #زرد
+os.system("clear")
+print("\033[32msalam")
+print()
+print()
+print(f"{red}")
+dark = """
+.xUHWH!! !!?MRDARK:.
+                        .X*#M@$!!  !X!M$$$$$$WWx:.
+                    :!!!!!!?H! :!$!$$$$$$$$$$8X:
+                    !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+                    :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+                    ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+                    !:~~~ .:!M"T#$$$$WX??#MRSALMAN!
+                {}      ~?WuxiW*   "#$$$$8!!!!??!!!
+                    :X- M$$$$       "T#$T~!8$WUXU~
+                    :%  ~#$$$m:        ~!~ ?$$$$$$
+                :!.-   ~T$$$$8xx.  .xWW- ~""##*"
+        .....   -~~:< !    ~?T#$$@@W@*?$$      /
+        W$@@M!!! .!~~ !!     .:XUW$W!~ "~:    :
+        #"~~.:x%!!  !H:   !WM$$$$Ti.: .!WUn+!
+        :::~:!!:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+        .~~   :X@!.-~   ?@WTWo("*$$$W$TH$!
+        Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+        $R@i.~~ !     :   ~$$$$$B$$en:`
+        ?MXT@Wx.~    :     ~"##*$$$$M~
+"""
+for bnr in dark:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.009)
+print()
+print(f"{green}")
+up = input("""
+(1)code filter account
+(2)code filter chaneel
+(3)code filter groop
+code filtering rubika√>>>""")
+time.sleep(2.0)
+print()
+
+if up == "1":
+	print("(1.2.4.6///g///f//2.4.5.7.8.9.0.9.7.5.3.1.1.1.3.4.6.7.8.0.0.8.6.5.3.2.2.1.1.3.5.6.8.8.5.4.3.1.2.4.6.7.7.8.8.9.8)")
+	
+if up == "2":
+	print("(1.3.4.6///f//d//8.9.0.8.6.5.3.2.1.2.4.5.7.9.0.9.7.6.4.6.7.9.0.7.6.4.2.1.2.4.5.7.8.9.0.7.5.7.9.0.9.7.5.4.2.1.2.3.5.6.7)")
+	
+if up == "3":
+	print("(1.4.6.8///f///b//1.3.5.6.7.8.9.0.9.7.6.4.2.1.1.2.4.6.7.8.0.9.7.5.3.2.1.1.3.5.6.8.0.0.8.6.4.2.1.1.3.5.6.8.9.0.8.6.5.4.5)")
+print()
+print()
+print("\033[31mamir ali jamili")
+print()
+print("\033[31msazaned = amir ali")
+print()
+print("\033[31mid shad = @Amirali_Jmili")
+print()
+print("\033[31mby by")
